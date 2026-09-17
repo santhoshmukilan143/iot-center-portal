13ca1d1298cbe98cd2c69081f866292f03a86fae
Santhosh <s32214840@gmail.com>
2026-09-17 23:52:20 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
 
+export const metadata: Metadata = {
+  title: "IoT Innovation Center",
+  description: "IoT Innovation & Research Center Portal",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
